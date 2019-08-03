fe17339fb70b6136fe5800a5e0fccce267eeee70
David Salgado <david@digitalronin.com>
2019-08-03 14:16:38 +0100

    Add SourcePanel and FormattedPanel components

---
+++ src/components/FormattedPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormattedPanel extends Component {
+  render() {
+    return (
+    <div className="formattedPanel">
+      Formatted panel
+    </div>
+    );
+  }
+}
+
+export default FormattedPanel;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import SourcePanel from './SourcePanel';
+import FormattedPanel from './FormattedPanel';
 
 class Main extends Component {
   render() {
     return (
       <div>
         Hello from Main
+        <SourcePanel />
+        <FormattedPanel />
       </div>
     );
   }
