47da5a1ffc5d3fb59ab0368c9be2e73f5f753b42
David Salgado <david@digitalronin.com>
2019-08-03 14:32:49 +0100

    Use Material UI Grid
    fixes #3

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import SourcePanel from './SourcePanel';
 import FormattedPanel from './FormattedPanel';
 
-class Main extends Component {
-  render() {
-    return (
-      <div>
-        Hello from Main
-        <SourcePanel />
-        <FormattedPanel />
-      </div>
-    );
-  }
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Main() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            Hello from Main
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <SourcePanel />
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <FormattedPanel />
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-export default Main;
-
